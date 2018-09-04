793b37e2df22103cde919a5effd6e26143311100
milimlee <37784073+milim2@users.noreply.github.com>
2018-09-03 22:04:21 -0400

    Add files via upload

--- CardioPractice/cardio2.js
+++ CardioPractice/cardio2.js
@@ -10,11 +10,18 @@ function chunkArray(arr, len) {
     let i = 0;
 
     // loop while index less than the array length
-    
+    while(i < arr.length) {
+        // Slice cut from the index to the index + the chunk length and push on to the chunked array
+        chunkedArr.push(arr.slice(i, i + len));
+        // Increment by chunk length
+        i += len;
+    }
+
+    return chunkedArr;
     
 }
 
 // Call function
-const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 2);
+const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 3);
 
 console.log(output);
