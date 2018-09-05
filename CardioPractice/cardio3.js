03e9b59fa52e35445be04f776eca25839c2dc217
milimlee <37784073+milim2@users.noreply.github.com>
2018-09-04 22:32:31 -0400

    array concat solution 1

---
+++ CardioPractice/cardio3.js
@@ -0,0 +1,15 @@
+// Challenge 3: Flatten Array
+// Take am array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7] 
+
+function flattenArray(arrays) {
+    // Solution 1
+    return arrays.reduce(function(a,b) {
+        return a.concat(b);
+    });
+}
+
+// Call function
+const output = flattenArray([[1, 2], [3, 4], [5, 6], [7]]);
+
+console.log(output);
