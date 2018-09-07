a63abb22acbe4016ea076c1277b168fb838cd4e2
milimlee <37784073+milim2@users.noreply.github.com>
2018-09-06 23:30:57 -0400

    add solution 2

--- CardioPractice/cardio2.js
+++ CardioPractice/cardio2.js
@@ -4,24 +4,47 @@
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1,2], [3, 4], [5, 6], [7]]
 
 function chunkArray(arr, len) {
+    // Solution 1
+
+    // // Init chunked arr
+    // const chunkedArr = [];
+    // // Set index
+    // let i = 0;
+
+    // // loop while index less than the array length
+    // while(i < arr.length) {
+    //     // Slice cut from the index to the index + the chunk length and push on to the chunked array
+    //     chunkedArr.push(arr.slice(i, i + len));
+    //     // Increment by chunk length
+    //     i += len;
+    // }
+
+    // return chunkedArr;
+    
+    // Solution 2
+
     // Init chunked arr
     const chunkedArr = [];
-    // Set index
-    let i = 0;
 
-    // loop while index less than the array length
-    while(i < arr.length) {
-        // Slice cut from the index to the index + the chunk length and push on to the chunked array
-        chunkedArr.push(arr.slice(i, i + len));
-        // Increment by chunk length
-        i += len;
-    }
+    // Loop through arr
+    arr.forEach(function(val) {
+        // Get last element
+        const last = chunkedArr[chunkedArr.length - 1];
 
+        // Check if last and if last length is equal to the chunk len
+        if (!last || last.length === len) {
+            chunkedArr.push([val]);
+        } else {
+            last.push(val);
+        }
+
+        //console.log(chunkedArr);
+        //console.log(last);
+    });
     return chunkedArr;
-    
 }
 
 // Call function
-const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 3);
+const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 2);
 
 console.log(output);
