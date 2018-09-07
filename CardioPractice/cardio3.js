4fc130a6a495881bbb88d732e83f47d3dcc625c4
milimlee <37784073+milim2@users.noreply.github.com>
2018-09-06 23:49:38 -0400

    flatten array

--- CardioPractice/cardio3.js
+++ CardioPractice/cardio3.js
@@ -4,15 +4,26 @@
 
 function flattenArray(arrays) {
     // Solution 1
-    // return arrays.reduce(function(a,b) {
-    //     return a.concat(b);
-    // });
+    // return arrays.reduce((a,b) => a.concat(b));
 
     // Solution 2
-    return [].concat.apply([], arrays);
+    // return [].concat.apply([], arrays);
+
+// Solution 3
+// function add(a, b, c) {
+// return a + b + c;
+// }
+// const arr = [1, 2, 3];
+// console.log(add(...arr));
+
+ return [].concat(...arrays);
 }
 
 // Call function
 const output = flattenArray([[1, 2], [3, 4], [5, 6], [7]]);
 
 console.log(output);
+
+
+
+
