af10510e8b6fcf38d24b895a134dadf20a8efcfe
milimlee <milimlee0@gmail.com>
2018-09-12 00:37:08 -0400

    Add files via upload

---
+++ CardioPractice/cardio5.js
@@ -0,0 +1,13 @@
+// Challenge 5: Letter changes
+// change every letter of the string to the one that follows it and captalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp Uifsf'
+
+
+
+
+// Call function
+// const output = isAnagram('elbow', 'belows');
+const output = isAnagram('Dormitory', 'dirty room###!!!$$$$');
+
+console.log(output);
